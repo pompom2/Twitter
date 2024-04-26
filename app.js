bfdcd1a4e32802f751dd2907c3eaf4b1b98b021c
pom_e <zmwaexp@gmail.com>
2024-04-26 15:03:07 +0900

    Twitter 서버 초기 세팅

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+import tweetsRouter from './router/tweets.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use('/tweets', tweetsRouter);   // 미들웨어
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+
+app.listen(8080);
+
