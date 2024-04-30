1f38163a92064c5a5faaf9361b78707e0cdc112b
pom_e <zmwaexp@gmail.com>
2024-04-30 14:41:11 +0900

    회원가입, 로그인 토큰 생성

--- controller/auth.js
+++ controller/auth.js
@@ -1,19 +1,49 @@
 import * as authRepository from '../data/auth.js';
+import bcrypt from 'bcrypt';
+import jswonwebtoken from 'jsonwebtoken';
 
+const secret = "abcd1234%^&*";
+
+async function makeToken(id){
+    const token = jswonwebtoken.sign({
+        id: id,
+        isAdmin: false
+    }, secret, {expiresIn: '1h'})
+    return token;
+}
+
+// 회원가입
 export async function signup(req, res, next){
-    const {username, password, name, email} = req.body;
-    const users = await authRepository.createUser(username, password, name, email);
+    const { username, password, name, email } = req.body;
+    const hashed =bcrypt.hashSync(password, 10);
+    const users = await authRepository.createUser(username, hashed, name, email);
     if(users){
         res.status(201).json(users);
     }
 }
-
-export async function login(req, res, next) {
-    const {username, password} = req.body;
+//로그인
+export async function login(req, res, next){
+    const { username, password } = req.body;
     const user = await authRepository.login(username);
+    // const bcryptPW = bcrypt.hashSync(password, 10);
+    // const result = bcrypt.compareSync('abcd1234', bcryptPW);
     if(user){
-        res.status(201).json(`${username} 로그인 완료`);
-    }else{
-        res.status(404).json({message: `${username}님 아이디 또는 비밀번호 확인하세요`});
+        if(bcrypt.compareSync(password, user.password)){
+        res.status(201).header('Token', makeToken(username)).json(`${username} 로그인 완료`)
+        }
+        else{
+            res.status(404).json({ message: `${username} 님 아이디 또는 비밀번호를 확인해주세요` });
+        }
+    }
+    // else{
+    //     res.status(404).json({ message: `${username} 님 아이디 또는 비밀번호를 확인해주세요` });
+    // }
+}
+
+//토큰이 있는지 여부, 로그인 흔적 확인
+export async function verify(req, res, next){
+    const token = req.header['Token'];
+    if(token){
+        res.status(200).json(token);
     }
 }
--- data/auth.js
+++ data/auth.js
@@ -1,28 +1,35 @@
 let users = [
     {
         id: '1',
-        username : 'apple',
-        password: '1111',
-        name: '김사과',
-        email: 'apple@apple.com',
-        url : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSRyel4MCk8BAbI6gT_j4DBTEIcY0WW4WWfoklymsWA&s'
+        // userid: "apple",
+        username: "apple",
+        password: "$2b$10$TvIhNWYtvCNzOA0ZkmNg6.YU3EG49msPLzxphLYahMT63DVWRWZTe",
+        name: "김사과",
+        email: "apple@apple.com",
+        url: "https://www.logoyogo.com/web/wp-content/uploads/edd/2021/02/logoyogo-1-45.jpg"
+    },
+    {
+        id: '2',
+        // userid: "banana",
+        username: "banana",
+        password: "$2b$10$TvIhNWYtvCNzOA0ZkmNg6.YU3EG49msPLzxphLYahMT63DVWRWZTe",
+        name: "반하나",
+        email: "banana@banana.com",
+        url: "https://img.freepik.com/premium-vector/banana-cute-kawaii-style-fruit-character-vector-illustration_787461-1772.jpg"
     }
 ]
-
 export async function createUser(username, password, name, email){
-    const user = {
-        id: '10',
+    const user ={
+        id: "10",
         username,
         password,
         name,
         email,
-        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSRyel4MCk8BAbI6gT_j4DBTEIcY0WW4WWfoklymsWA&s'
+        url: "https://www.logoyogo.com/web/wp-content/uploads/edd/2021/02/logoyogo-1-45.jpg"
     }
     users = [user, ...users]
     return users;
 }
-
 export async function login(username){
-    const user = users.find((user) => user.username === username)
-    return user;
+    return users.find((users) => users.username === username);
 }
